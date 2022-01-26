097befb55c07e04dd97b316f66bf3798f3ce4191
Daniel Zhang-Li <1098811384@qq.com>
2022-01-26 15:14:52 +0800

    push code for backend

---
+++ backend.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from TriMention.web import parse_mentions
+from MCMention.applyMention import topkMention
+from MCLinking.applyMC_Linking import topkSubTitle,topkSubTitleWithScores
+from MCRelation.applyRelation import topkRelation
+
+import json
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] =False
+@app.route("/readyToUse/<string:qs>")
+def useAllFeatures(qs):
+	mentions = parse_mentions(qs)
+	mentionDetection = topkMention(qs,mentions,K=3)
+	disambiguationBySubTitle = topkSubTitle(qs,mentionDetection,K=3)
+	disambiguationByRelation = list(topkRelation(qs,disambiguationBySubTitle,K=1)[0])
+	disambiguationByRelation[2] = float(disambiguationByRelation[2])
+	return {"data":disambiguationByRelation}
+
+app.run(host="localhost",port=9899)
