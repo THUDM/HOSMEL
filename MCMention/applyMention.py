8e125da51fa03711cc46649cd679c8abed983190
Daniel Zhang-Li <1098811384@qq.com>
2022-01-26 13:29:58 +0800

    push code for Mention disambiguation

---
+++ MCMention/applyMention.py
@@ -0,0 +1,31 @@
+from transformers import AutoTokenizer, AutoModelForMultipleChoice
+from tqdm import tqdm
+import torch
+import os
+device="cuda:2"
+
+
+model = None
+tokenizer = None
+checkpoint_num = 1500
+# model_location = os.path.join(os.path.dirname(__file__),"model/macbert-linking-chinese-baidu-add-alias-v4-lr_1e-5-wd_1e-3-eligant_data4_20/checkpoint-%d"%(checkpoint_num))
+model_location = os.path.join(os.path.dirname(__file__),"model/macbert-pipel-mention-chinese/checkpoint-%d"%(checkpoint_num))
+
+print("Loading Mention Model")
+tokenizer = AutoTokenizer.from_pretrained(model_location,local_files_only=True,use_fast=True)
+model = AutoModelForMultipleChoice.from_pretrained(model_location,local_files_only=True).eval().to(device)
+print("Mention Model Loaded")
+
+def topkMention(q,mentions,K=3):
+	questions = [q]*len(mentions)
+	tokenized = tokenizer(questions,mentions,padding=True,truncation=True,return_tensors="pt",max_length=128)
+	tokenized = {k:v.to(device) for k,v in tokenized.items()}
+	
+	returned = model(**{k:v.unsqueeze(0) for k,v in tokenized.items()})
+	logits = returned.logits[0].cpu().detach().numpy()
+	mentions = [(mentions[i],logits[i]) for i in range(len(mentions))]
+	mentions.sort(key=lambda x:x[1],reverse=True)
+	return mentions[:K]
+
+def filterMention(mentionDetection,mentions):
+	return {k[0]:mentions[k[0]] for k in mentionDetection}
---
+++ MCMention/preprocessData.py
@@ -0,0 +1,38 @@
+from transformers import AutoTokenizer, AutoModelForMultipleChoice, TrainingArguments, Trainer
+import pickle
+from datasets import load_dataset
+batch_size = 32
+log_rate = 10
+model_name = "hfl/chinese-macbert-large"
+tokenizer = AutoTokenizer.from_pretrained(model_name,use_fast=True)
+model = AutoModelForMultipleChoice.from_pretrained(model_name)
+
+data = load_dataset("json",data_files={"train":'./Top10ForMention.json'},cache_dir="../cache/")
+data = data["train"]
+# sample data
+data = data.shuffle()
+data = data.shard(num_shards=95,index=0)
+ending_names = ["target0","target1","target2","target3"]
+def preprocess(example):
+	first_sentences = [[context]*4 for context in example["sentence"]]
+	question_headers = ["" for mention in example["mention"]]
+	second_sentences = [[header+example[end][i] for end in ending_names] for i,header in enumerate(question_headers)]
+	first_sentences = sum(first_sentences,[])
+	second_sentences = sum(second_sentences,[])
+	tokenized = tokenizer(first_sentences,second_sentences,truncation=True,max_length=128)
+	tokenized = {k:[v[i:i+4] for i in range(0, len(v), 4)] for k,v in tokenized.items()}
+	return tokenized
+
+def preprocess_no_batch(example):
+	first_sentences = [example["sentence"]]*4
+	question_header = example["mention"]+"是"
+	second_sentences = [question_header+example[end] for end in ending_names]
+	tokenized = tokenizer(first_sentences,second_sentences,truncation=True,max_length=128)
+	tokenized["label"] = example["Label"]
+	return tokenized
+c_names = data.column_names
+c_names.remove("Label")
+encoded_data = data.map(preprocess,batched=True,remove_columns=c_names,num_proc=4).rename_column("Label","label")
+print(encoded_data[0])
+print(data[0])
+encoded_data.to_json("./processedData.json")
